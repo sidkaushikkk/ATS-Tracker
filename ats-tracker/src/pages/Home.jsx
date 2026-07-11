61d26da0a708456111495ff353edcf82db6d8eab
Sidhant Kaushik <ksiddhant705@gmail.com>
2026-07-11 23:13:48 +0530

    Made theme changes and new animations in Box.jsx

--- ats-tracker/src/pages/Home.jsx
+++ ats-tracker/src/pages/Home.jsx
@@ -1,14 +1,35 @@
 import Navbar from "../components/Navbar/Navbar";
 import Box from "../components/Box/Box";
 
-function App() {
+import HowItWorks   from "../components/HomeSections/HowItWorks";
+import Features     from "../components/HomeSections/Features";
+import ATSReasons   from "../components/HomeSections/ATSReasons";
+import ReportPreview from "../components/HomeSections/ReportPreview";
+import BeforeAfter  from "../components/HomeSections/BeforeAfter";
+import WhyChoose    from "../components/HomeSections/WhyChoose";
+import Formats      from "../components/HomeSections/Formats";
+import FAQ          from "../components/HomeSections/FAQ";
+import FinalCTA     from "../components/HomeSections/FinalCTA";
+import Footer       from "../components/HomeSections/Footer";
 
+function Home() {
   return (
     <>
-    <Navbar></Navbar>
-    <Box></Box>
+      <Navbar />
+      <Box />
+
+      <HowItWorks />
+      <Features />
+      <ATSReasons />
+      <ReportPreview />
+      <BeforeAfter />
+      <WhyChoose />
+      <Formats />
+      <FAQ />
+      <FinalCTA />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default Home;
