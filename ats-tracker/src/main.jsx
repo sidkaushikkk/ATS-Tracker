d7897a5ff366cc16bb90a5abff38de774b89f71c
Sidhant Kaushik <ksiddhant705@gmail.com>
2026-07-25 16:33:17 +0530

    not an important commit yet again

--- ats-tracker/src/main.jsx
+++ ats-tracker/src/main.jsx
@@ -5,7 +5,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.jsx'
 
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || 'your_google_client_id_here';
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '942272954043-iojhqs9833umf9dj3o9mqa48kon96is8.apps.googleusercontent.com';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
