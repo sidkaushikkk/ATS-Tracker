5eba27cac92e4fd7a160b426fa08e252420fda1e
Sidhant Kaushik <ksiddhant705@gmail.com>
2026-07-26 19:30:26 +0530

    updated user.js acc to new login functionality

--- ats-tracker/backend/models/User.js
+++ ats-tracker/backend/models/User.js
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  googleId: { type: String, required: true, unique: true },
+  googleId: { type: String, sparse: true, unique: true },
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
+  emailVerified: { type: Boolean, default: false },
+  authProviders: { type: [String], default: [] },
   profilePicture: { type: String },
   collegeName: { type: String },
   degree: { type: String },
